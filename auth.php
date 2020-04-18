8e113f09360aff9b19f4a5f350f6ecb6d8eaefa4
しまなみさん <mikan.sup.all@gmail.com>
2020-04-19 07:44:01 +0900

    ログイン認証

---
+++ auth.php
@@ -0,0 +1,45 @@
+<?php
+//================================
+// ログイン認証・自動ログアウト
+//================================
+
+// ログインしている場合
+if(!empty($_SESSION['login_date'])){
+    debug('ログイン済みユーザーです。 auth.php');
+
+    // 現在日時が最終ログイン+有効期限を超えていた場合
+    if( ($_SESSION['login_date'] + $_SESSION['login_limit']) < time() ){
+        debug('ログイン有効期限を超えています。 auth.php');
+
+        // セッションの削除（ログアウト処理）
+        session_destroy();
+        // ログインページへ遷移
+        header("Location:login.php");
+        // ページ遷移後、後続の処理を行わにようにする
+        return;
+    }else{
+        debug('ログイン有効期限内です。 auth.php');
+        // 最終ログイン日時を現在に変更
+        $_SESSIION['login_date'] = time();
+
+        // login.phpにアクセスしてきた場合に、マイページへ遷移させる
+        // $_SERVER['PHP_SELF']はURLのパスを返す。さらにbasenameメソッドを使うことで「login.php」といったファイル名を抜粋する
+        if(basename($_SERVER['PHP_SELF']) === 'login.php'){
+            debug('アクセスしたパスを確認 auth.php：'. print_r($_SERVER['PHP_SELF']));
+            debug('マイページへ遷移します auth.php');
+            // マイページへ遷移
+            header("Location:mypage.php");
+        }
+    }
+
+// 未ログインユーザーの処理
+}else{
+    debug('未ログインユーザーです auth.php');
+    // 未ログインユーザーが、ログイン認証が必要なページにアクセスしていたらログインページに遷移させる
+    if(basename($_SERVER['PHP_SELF']) !== 'login.php'){
+        // ログインページへ遷移
+        header("Location:login.php");
+    }
+}
+
+?>
--- login.php
+++ login.php
@@ -1,4 +1,105 @@
+<?php
+// 共通関数
+require('function.php');
 
+debug('=============================');
+debug('ここはログインページです login.php');
+debug('=============================');
+debugLogStart();
+
+// ログイン認証
+require('auth.php');
+
+//================================
+// ログイン画面処理
+//================================
+// POSTされていた場合
+if(!empty($_POST)){
+  debug('POST送信されています login.php');
+  // 変数にユーザー情報を格納
+  $email = $_POST['email'];
+  $pass = $_POST['pass'];
+  // ショートハンド（省略法）とう書き方
+  // チェックボックスは真偽値で入ってくる
+  $pass_save = (!empty($_POST['pass_save'])) ? true : false ;
+  debug('チェックボックスの真偽値を確認 login.php' . print_r($_POST['pass_save']));
+
+  // 未入力チェック
+  validRequired($email, 'email');
+  validRequired($pass, 'pass');
+
+  // emailの形式チェック
+  validEmail($email, 'email');
+  // emailの最大文字数チェック
+  validMaxLen($email, 'email');
+
+  // パスワードの半角英数字チェック
+  validHalf($pass, 'pass');
+  // パスワードの最大文字数チェック
+  validMaxLen($pass, 'pass');
+  // パスワードの最小文字数チェック
+  validMinLen($pass, 'pass');
+
+  // エラーメッセージ変数が空であればバリデーションOK
+  if(empty($err_msg)){
+    debug('バリデーションOKです login.php');
+    
+    //例外処理
+    try {
+      // DBへ接続
+      $dbh = dbConnect();
+      // SQL文作成
+      $sql = 'SELECT password, id FROM users WHERE email = :email AND delete_flg = 0';
+      // プレースホルダーを使用
+      $data = array(':email' => $email);
+      // クエリ実行
+      $stmt = queryPost($dbh, $sql, $data);
+      // クエリ結果の値を取得
+      $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      // debug('クエリ結果の中身 login.php：'.print_r($result,true));
+      
+      // パスワード照合
+      if(!empty($result) && password_verify($pass, array_shift($result))){
+        debug('パスワードがマッチしました login.php');
+        //password_verify:ハッシュ化したパスワードと入力したパスワードを比べるための関数
+        
+        //ログイン有効期限（デフォルトを１時間とする）
+        $sesLimit = 60*60;
+        // 最終ログイン日時を現在日時に
+        $_SESSION['login_date'] = time();
+        
+        // ログイン保持にチェックがある場合
+        if($pass_save){
+          debug('ログイン保持にチェックがあります login.php');
+          // ログイン有効期限を30日にしてセット
+          $_SESSION['login_limit'] = $sesLimit * 24 * 30;
+        }else{
+          debug('ログイン保持にチェックはありません login.php');
+          // 次回からログイン保持しないので、ログイン有効期限を1時間後にセット
+          $_SESSION['login_limit'] = $sesLimit;
+        }
+        // ユーザーIDを格納
+        $_SESSION['user_id'] = $result['id'];
+        
+        debug('セッション変数の中身だよ！！ login.php：'.print_r($_SESSION,true));
+        debug('マイページへ遷移します login.php');
+        header("Location:mypage.php"); //マイページへ
+      }else{
+        debug('パスワードがアンマッチです login.php');
+        $err_msg['common'] = MSG09;
+      }
+
+    } catch (Exception $e) {
+      error_log('エラー発生:' . $e->getMessage());
+      $err_msg['common'] = MSG07;
+    }
+  }
+}
+
+
+
+?>
 <?php
   $siteTitle = 'HOME';
   require('head.php');
@@ -19,27 +120,40 @@
 
        <div class="form-container">
         
-         <form action="mypage.html" class="form">
+       <form action="" method="post" class="form">
            <h2 class="title">ログイン</h2>
            <div class="area-msg">
-             メールアドレスまたはパスワードが違います
+             <?php 
+              if(!empty($err_msg['common'])) echo $err_msg['common'];
+             ?>
            </div>
-           <label>
+           <label class="<?php if(!empty($err_msg['email'])) echo 'err'; ?>">
             メールアドレス
-             <input type="text" name="email">
+             <input type="text" name="email" value="<?php if(!empty($_POST['email'])) echo $_POST['email']; ?>">
            </label>
-           <label>
+           <div class="area-msg">
+             <?php 
+             if(!empty($err_msg['email'])) echo $err_msg['email'];
+             ?>
+           </div>
+           <label class="<?php if(!empty($err_msg['pass'])) echo 'err'; ?>">
              パスワード
-             <input type="text" name="pass">
+             <input type="password" name="pass" value="<?php if(!empty($_POST['pass'])) echo $_POST['pass']; ?>">
            </label>
+           <div class="area-msg">
+             <?php 
+             if(!empty($err_msg['pass'])) echo $err_msg['pass'];
+             ?>
+           </div>
            <label>
              <input type="checkbox" name="pass_save">次回ログインを省略する
            </label>
             <div class="btn-container">
               <input type="submit" class="btn btn-mid" value="ログイン">
             </div>
-            パスワードを忘れた方は<a href="passRemindSend.html">コチラ</a>
+            パスワードを忘れた方は<a href="passRemindSend.php">コチラ</a>
          </form>
+
        </div>
 
       </section>
