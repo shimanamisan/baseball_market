5ece843779d81eefea0f69a796cd710fbc02a279
しまなみさん <mikan.sup.all@gmail.com>
2020-04-30 23:04:43 +0900

    連絡掲示板の販売者を判定する処理を追加

--- mypage.php
+++ mypage.php
@@ -14,21 +14,31 @@ require('auth.php');
 //===============================
 //画面表示用データ取得
 //===============================
+$partnerUserId = '';
+$partnerUserInfo = '';
+$myUserInfo = '';
+
 $u_id = $_SESSION['user_id'];
-debug('SESSION[id]のユーザーIDを確認しています mypage.php：'.print_r($u_id,true));
+// debug('SESSION[id]のユーザーIDを確認しています mypage.php：'.print_r($u_id,true));
 // DBから商品データ取得
 $productData = getMyProducts($u_id);
 // DBから連絡掲示板データ取得
 $bordData = getMyMsgsAndBord($u_id);
 // DBからお気に入りデータを取得
 $likeData = getMyLike($u_id);
-// 販売者のユーザー情報を取得
-if(!empty($bordData)){
-    $buyUser = getUser($bordData[0]['sale_user']);
-    debug('相手のユーザー情報です'.print_r($buyUser, true));
-}
 
 
+
+
+// // 相手のユーザーIDが入っているかチェックする
+// if(isset($partnerUserId)){
+//     error_log('エラー発生：相手のユーザー情報が取得できませんでした mypage.php');
+//     header("Location:mypage.php"); // マイページへ
+//     exit();
+// }
+
+// カレントページ
+// $currentPageNum = (!empty($_GET['p'])) ? $_GET['p'] : 1; // デフォルトは1ページめ
 
 // DBからきちんと全てのデータが取れているかのチェックは行わず、取れなければ何も表示しないこととする
 // debug('取得した掲示板データ mypage.php：'.print_r($productData, true));
@@ -50,14 +60,14 @@ require('head.php');
   require('header.php');
   ?>
 
-  <p id="js-show-msg" style="display:none; line-height:90px;" class="msg-slide">
-      <?php echo getSessionFlash('msg_success');?> 
-  </p>
+    <p id="js-show-msg" class="msg-slide msg-success">
+          <?php echo getSessionFlash('msg_success'); ?>
+    </p>
 
         <!-- メインコンテンツ -->
         <div id="contents" class="site-width">
           
-          <h1 class="page-title">MYPAGE</h1>
+          <h1 class="page-title">マイページ</h1>
 
           <!-- Main -->
           <section id="main" >
@@ -89,6 +99,11 @@ require('head.php');
               <h2 class="title">
                 <span class="title-header">登録商品一覧連絡掲示板一覧</span>
               </h2>
+
+              <?php
+              if(!empty($bordData)){
+              ?>
+
               <table class="table">
                 <thead>
                   <tr>
@@ -98,43 +113,76 @@ require('head.php');
                   </tr>
                 </thead>
                 <tbody>
+
                   <?php
                     if(!empty($bordData)){
                       foreach($bordData as $key => $val){
+
+                      $dealUserIds[] = $val['buy_user'];
+                      $dealUserIds[] = $val['sale_user'];
+                      debug('購入者と販売者のID $dealUserIds：'.print_r($dealUserIds, true));
+                      // ログインユーザーが、sale_usr若しくはbuy_userと同じでは無いことを確認（）
+                      if(($key = array_search($_SESSION['user_id'], $dealUserIds)) !== false){
+                          debug('unset前の変数です：'. print_r($dealUserIds, true));
+                          unset($dealUserIds[$key]);
+                          debug('unsetしています：'. print_r($dealUserIds, true));
+                        }
+                      // 配列からユーザーIDを取り出す
+                      $partnerUserId = array_shift($dealUserIds);
+                      debug('取得した相手のユーザーID：' .$partnerUserId);
+                      $partnerUser = getUser($partnerUserId);
+                     
                         if(!empty($val['msg'])){
                           $msg = array_shift($val['msg']);
-                          // debug('ボードデータです mypage.php：'.print_r($bordData,true));
+                          debug('分解後の$val mypage.php：'.print_r($val,true));
+                          // debug('ボードデータです mypage.php：'.print_r($msg,true));
                   ?>
+
                   <tr>
                       <td><?php echo sanitize(date('Y/m/d H:i', strtotime($msg['send_date']))); ?></td>
-                      <td><?php echo sanitize($buyUser['username']);?></td>
+                      <td><?php echo sanitize($partnerUser['username']);?></td>
                       <td><a href="msg.php?m_id=<?php echo sanitize($val['id']); ?>"><?php echo mb_substr(sanitize($msg['msg']),0,40); ?></a></td>
                   </tr>
+
                   <?php
                         }else{
                   ?>
+
                   <tr>
                     <td>-- --</td>
-                    <td>○○ ○○</td>
-                    <a href="msg.php?m_id=<?php echo sanitize($val['id']); ?>">まだメッセージはありません</a></td>
+                    <td><?php echo sanitize($partnerUser['username']);?></td>
+                    <td><a href="msg.php?m_id=<?php echo sanitize($val['id']); ?>">まだメッセージはありません</a></td>
+
                   <?php
                       }
                     }
                   }
                   ?>
+
                   </tr>
                  </tbody>
               </table>
+
+              <?php
+              }else{
+              ?>
+
+          <p style="text-align:left;">メッセージはありません。</p>
+
+          <?php
+          }
+          ?>
             </section>
-            
             <section class="list panel-list">
               <h2 class="title">
                 <span class="title-header">登録商品一覧お気に入り一覧</span>
               </h2>
+
               <?php
-                if(!empty($likeData)):
-                  foreach($likeData as $key => $val):
+                if(!empty($likeData)){
+                  foreach($likeData as $key => $val){
               ?>
+
               <a href="productDetail.php<?php echo (!empty(appendGetParam())) ? appendGetParam().'&p_id='.$val['id'] : '?p_id='.$val['id'] ?>" class="panel">
                 <div class="trim">
                   <div class="panel-head">
@@ -144,11 +192,16 @@ require('head.php');
                 <div class="panel-body">
                 <p class="panel-title"><?php echo sanitize($val['name']); ?> <span class="price">¥<?php echo sanitize(number_format($val['price'])); ?></span></p>
                 </div>
+
               <?php
-                endforeach;
-              endif;
+                }
+              }else{
               ?>
 
+              <p style="text-align:left;">お気に入り登録はありません。</p>
+        <?php
+          }
+        ?>
             </section>
           </section>
       
